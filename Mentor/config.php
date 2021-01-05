412dd59b53bdfd21e134366e1ea9d29b29a3b3ae
Udaykumar <udayvishal8125@gmail.com>
2021-01-05 18:52:20 +0530

    05/01/2021 Verion 1.10

--- Mentor/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$work_dir = "../";
-// Require Variables
-	$functions = "C:/xampp/htdocs/Uday/Mentor/functions/function.php";
-		$login = "../pages/login.php";
-
-
-
- ?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# ELS_Insi_FS_project_01
-Mini Project - E Learning Managing System
